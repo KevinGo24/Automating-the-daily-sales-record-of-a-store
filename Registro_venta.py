8b825a2b2afe11222b9ee76562cb5be4fe477f4d
kevin <kevinG>
2026-03-10 19:18:56 -0500

    se hizo el registro

--- Registro_venta.py
+++ Registro_venta.py
@@ -1,13 +1,14 @@
-import Solicitar_usuario fro
 
-for i in range(cantidad_productos):
-    print(f"\n--- Registro del Producto #{i + 1} ---")
+productos = int(input("¿Cuanta compra va a realizar ? "))
+def registrar_ventas():
     
-    # Pedimos los 3 datos necesarios
-    nombre = input("Nombre del producto: ")
-    precio = float(input(f"Precio unitario de {nombre}: "))
-    cantidad = int(input(f"Cantidad de unidades vendidas: "))
-    
-    # Aquí iría tu lógica para guardar o calcular
-    subtotal = precio * cantidad
+    for i in range(productos):
+        print(f"\n--- Registro del Producto #{i + 1} ---")
+        # Pedimos los 3 datos necesarios
+        nombre = input("Nombre del producto: ")
+        precio = float(input(f"Precio unitario de {nombre}: "))
+        cantidad = int(input("Cantidad de unidades vendidas: "))
+        
+        # Aquí iría tu lógica para guardar o calcular
+        subtotal = precio * cantidad
     print(f"Subtotal de esta venta: ${subtotal}")
